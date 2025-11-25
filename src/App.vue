bb7a9584ed20d8cb2b9d272bd0932654f4bdc661
Frame Zen Owolden <2086243422@qq.com>
2025-11-25 18:59:21 +0800

    Add files via upload
    上传代码

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<!--html结构-->
+    <List/>
+</template>
+
+<script lang="ts">
+    import List from './components/List.vue';
+
+//JS或TS
+    export default{
+        name:'App',
+        components:{List}
+    }
+</script>
+
+<style>
+/*样式*/
+
+</style>
